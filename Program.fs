7d9d4317c2971df60276dbfa1ab78c395db17530
Charlie <robo-wiggles@curylo.org>
2017-04-23 21:59:03 -0400

    Will speak words (Mac) when 'h' is pressed.

--- Program.fs
+++ Program.fs
@@ -2,6 +2,9 @@
 
 let level1 = System.IO.File.ReadAllLines "Level1.txt"
 let level2 = System.IO.File.ReadAllLines "Level2.txt"
+let level3 = System.IO.File.ReadAllLines "Level3.txt"
+let level4 = System.IO.File.ReadAllLines "Level4.txt"
+let level5 = System.IO.File.ReadAllLines "Level5.txt"
 
 [<EntryPoint>]
 let main argv =
@@ -11,18 +14,44 @@ let main argv =
     printfn "Hello %s from Sight Words!" name
     let random = Random ()
     let rec loop (words:string array) = 
-        printfn "Press enter to get a sight word."
-        Console.ReadLine () |> ignore
-        words.[random.Next(words.Length)]
-        |> printfn "%s"
-        Console.WriteLine ()
-        loop words
+        let randomWord = words.[random.Next(words.Length)]
+        printfn "%s" randomWord
+        Console.ReadLine ()
+        |> function
+        | "q" | "Q" -> printfn "Quitting this level."
+        | "h" | "H" -> 
+            let p = System.Diagnostics.Process.Start ("say", randomWord)
+            p.WaitForExit () |> ignore
+            loop words
+        | _ ->
+            loop words
     let rec selectLevel () =
-        printfn "Which level do you want, 1 or 2?"
+        printfn "Which level do you want, 1, 2, 3, 4, or 5?"
         let selection = Console.ReadLine ()
         match selection with
-        | "1" -> loop level1
-        | "2" -> loop level2
-        | _ -> printfn "I don't know that level."; selectLevel ()
+        | "1" ->
+            printfn "Press enter to get a sight word."
+            loop level1
+            selectLevel ()
+        | "2" ->
+            printfn "Press enter to get a sight word."
+            loop level2
+            selectLevel ()
+        | "3" ->
+            printfn "Press enter to get a sight word."
+            loop level3
+            selectLevel ()
+        | "4" ->
+            printfn "Press enter to get a sight word."
+            loop level4
+            selectLevel ()
+        | "5" ->
+            printfn "Press enter to get a sight word."
+            loop level5
+            selectLevel ()
+        | "q" | "Q" -> printfn "Quitting application.  We can learn more later."
+        | _ -> 
+            printfn "I don't know that level."
+            selectLevel ()
     selectLevel ()
     0
